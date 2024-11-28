30d2cdd845d51364a02c0f5073a6ceafe8737650
Eg0r0k <egokakill@gmail.com>
2024-11-28 14:20:26 +0300

    Config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+    "@nuxthq/studio",
+  ],
+  studio: {
+    gitInfo: {
+      name: "partfolio",
+      owner: "Eg0r0k",
+      url: "https://github.com/Eg0r0k/partfolio",
+    },
+  },
   i18n: {
     locales: [
       { code: "en", language: "en-US" },
       { code: "ru", language: "ru-RU" },
     ],
-    defaultLocale:"ru",
+    defaultLocale: "ru",
   },
 });
